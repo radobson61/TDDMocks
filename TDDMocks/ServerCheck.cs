b9143d1e41e6f3ab48ddf46df7e284e81e6a9d7c
Bob Dobson <ra_dobson@hotmail.com>
2014-08-29 11:23:33 -0700

    trigger git

---
+++ TDDMocks/ServerCheck.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDMocks
+{
+    public enum ServerConfiguration { OK, Error, ReportingError, RapidChange};
+
+    public class ServerCheck
+    {
+        DataTable _serverdata;
+        public ServerCheck(DataTable table)
+        {
+            _serverdata = table;
+        }
+
+        public ServerConfiguration Result(string servername)
+        {
+            string targetserver = (from serverrow in _serverdata.AsEnumerable()
+                                 where serverrow.Field<string>("ServerName") == servername
+                                 select (string)serverrow["ServerName"]).LastOrDefault();
+
+            DateTime lastCollection = (from serverrow in _serverdata.AsEnumerable()
+                                       where serverrow.Field<string>("ServerName") == servername
+                                       select (DateTime)serverrow["CollectionTime"]).LastOrDefault();
+
+            if (string.IsNullOrEmpty(targetserver))
+                return ServerConfiguration.Error;
+            if (DateTime.Now.Subtract(TimeSpan.FromHours(12)) > lastCollection)
+                return ServerConfiguration.ReportingError;
+            if (PercentageChange(servername) > 15)
+                return ServerConfiguration.RapidChange;            
+            return ServerConfiguration.OK;
+
+        }
+
+        private int PercentageChange(string servername)
+        {
+            decimal measure1;
+            decimal measure2;
+            int c = 0;
+            IEnumerable<DataRow> results = _serverdata.AsEnumerable()
+                .Where(s => s.Field<string>("ServerName") == servername)
+                .OrderByDescending(o => o.Field<DateTime>("CollectionTime"));
+            if (results != null && results.Count<DataRow>() > 1)
+            {
+                measure1 = (decimal)results.ElementAt(0).Field<Int32>("Measurement");
+                measure2 = (decimal)results.ElementAt(1).Field<Int32>("Measurement");
+                decimal r = (measure1 / measure2);
+                c = (int)(r * 100) - 100;
+            }
+
+            return c;
+
+        }
+    }
+}
---
+++ TDDMocksTest/ServerCheckerTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+using TDDMocks; 
+
+namespace TDDMocksTest
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class ServerCheckerTests
+    {
+        ///<summary>
+        /// ServerConfigurationResult Is Error when ServerName is NotFoundinDB
+        ///</summary>
+        ///<remarks>AC 1. When there is no entry in the database for a monitored server name I want a server configuration error message</remarks>
+        [TestMethod()]
+        [TestCategory("Programmer")]
+        public void ServerConfigurationResult_Is_Error_When_ServerName_is_NotFoundinDB()
+        {
+            //assemble
+            string servername = "Server";
+            DataTable table = MakeServerInformationTable();
+            DataRow dr;
+            dr = table.NewRow();
+            dr["ServerName"] = string.Concat(servername);
+            dr["CollectionTime"] = DateTime.Now;
+            dr["Measurement"] = 0;
+            table.Rows.Add(dr);
+
+            ServerConfiguration expected = ServerConfiguration.Error;
+            ServerCheck target = new ServerCheck(table);
+            //act
+            ServerConfiguration actual = target.Result(string.Concat(servername, "A"));
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+        ///<summary>
+        /// ServerConfigurationResult Is ReportingError when LastDBEntry is MoreThan12HoursOld
+        ///</summary>
+        ///<remarks>AC 2. When the most recent entry in the database is more than 12 hours old I want a server reporting error message</remarks>
+        [TestMethod()]
+        [TestCategory("Programmer")]
+        public void ServerConfigurationResult_Is_ReportingError_When_LastDBEntry_is_MoreThan12HoursOld()
+        {
+            //assemble
+            string servername = "Server";
+            DataTable serverInformation = MakeServerInformationTable();
+            DataRow dr;
+            dr = serverInformation.NewRow();
+            dr["ServerName"] = string.Concat(servername);
+            dr["CollectionTime"] = DateTime.Now.Subtract(TimeSpan.FromHours(13));
+            dr["Measurement"] = 0;
+            serverInformation.Rows.Add(dr);
+            ServerConfiguration expected = ServerConfiguration.ReportingError;
+            ServerCheck target = new ServerCheck(serverInformation);
+            //act
+            ServerConfiguration actual = target.Result(servername);
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        ///<summary>
+        /// ServerCheckResult Is RapidChange when RecentValue is 15PercentGreaterThanPrevious
+        ///</summary>
+        ///<remarks>3. When the most recent error in the database is more than 15% above the average I want rapid change information message.</remarks>
+        [TestMethod()]
+        [TestCategory("Programmer")]
+        public void ServerCheckResult_Is_RapidChange_When_RecentValue_is_15PercentGreaterThanPrevious()
+        {
+            //assemble
+            string servername = "Server";
+            DataTable serverInformation = MakeServerInformationTable();
+            DataRow dr;
+            dr = serverInformation.NewRow();
+            // older measurement
+            dr["ServerName"] = servername;
+            dr["CollectionTime"] = DateTime.Now.Subtract(TimeSpan.FromHours(13));
+            dr["Measurement"] = 100;
+            serverInformation.Rows.Add(dr);
+            dr = serverInformation.NewRow();
+            dr["ServerName"] = servername;
+            dr["CollectionTime"] = DateTime.Now.Subtract(TimeSpan.FromHours(2));
+            dr["Measurement"] = 116;
+            serverInformation.Rows.Add(dr);
+            ServerConfiguration expected = ServerConfiguration.RapidChange;
+            ServerCheck target = new ServerCheck(serverInformation);
+            //act
+            ServerConfiguration actual = target.Result(servername);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private DataTable MakeServerInformationTable()
+        {
+            DataTable table = new DataTable("ServerInformation");
+            DataColumn dc = new DataColumn() { DataType = System.Type.GetType("System.String"), ColumnName = "ServerName" };
+            table.Columns.Add(dc);
+            dc = new DataColumn() { DataType = System.Type.GetType("System.DateTime"), ColumnName = "CollectionTime" };
+            table.Columns.Add(dc);
+            dc = new DataColumn() { DataType = System.Type.GetType("System.Int32"), ColumnName = "Measurement" };
+            table.Columns.Add(dc);
+            return table;
+        }
+
+
+    }
+}
