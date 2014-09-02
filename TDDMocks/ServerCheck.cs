7dc6a55e25b63c597c399d4c3079bcad615bd41a
Bob Dobson <radobson61@gmail.com>
2014-09-02 10:07:20 -0700

    Refactoring ServerCheck raises Maintainability Index to 87.

--- TDDMocks/ServerCheck.cs
+++ TDDMocks/ServerCheck.cs
@@ -20,10 +20,8 @@ namespace TDDMocks
         public ServerConfiguration Result(string servername)
         {
             IEnumerable<DataRow> results = _serverdata.AsEnumerable()
-    .Where(s => s.Field<string>("ServerName") == servername)
-    .OrderByDescending(o => o.Field<DateTime>("CollectionTime"));
-
-
+                .Where(s => s.Field<string>("ServerName") == servername)
+                .OrderByDescending(d => d.Field<DateTime>("CollectionTime"));
 
             return FindResult(results);
 
@@ -31,7 +29,6 @@ namespace TDDMocks
 
         private ServerConfiguration FindResult(IEnumerable<DataRow> results)
         {
-            ServerConfiguration result = ServerConfiguration.OK;
 
             if (results.Count() == 0)
                 return ServerConfiguration.Error;
@@ -39,26 +36,21 @@ namespace TDDMocks
                 return ServerConfiguration.ReportingError;
             if (PercentageChange(results.First().Field<string>("ServerName"), results) > 15)
                 return ServerConfiguration.RapidChange;
-            
-            return result;
+
+            return ServerConfiguration.OK;
         }
 
         private int PercentageChange(string servername, IEnumerable<DataRow> results)
         {
-            decimal measure1;
-            decimal measure2;
-            int c = 0;
 
   
             if (results.Count<DataRow>() > 1)
             {
-                measure1 = (decimal)results.ElementAt(0).Field<Int32>("Measurement");
-                measure2 = (decimal)results.ElementAt(1).Field<Int32>("Measurement");
-                decimal r = (measure1 / measure2);
-                c = (int)(r * 100) - 100;
+                decimal r = ((decimal)results.ElementAt(0).Field<Int32>("Measurement") / (decimal)results.ElementAt(1).Field<Int32>("Measurement"));
+                return (int)(r * 100) - 100;
             }
 
-            return c;
+            return 0;
 
         }
 
